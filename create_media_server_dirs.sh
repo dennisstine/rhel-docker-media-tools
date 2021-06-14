46f83f88c5599cddbb099821fb7e862095147682
Dennis Stine <dennis.stine@gmail.com>
2021-06-14 00:22:13 -0500

    Create create_media_server_dirs.sh

---
+++ create_media_server_dirs.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+home_dir="/media_server"
+configs_dir="$home_dir/configs"
+plex_dir="$home_dir/plex"
+sabnzbd_dir="$home_dir/sabnzbd"
+sonarr_dir="$home_dir/sonarr"
+radarr_dir="$home_dir/radarr"
+
+mkdir -p "$home_dir"
+
+mkdir -p "$configs_dir"
+mkdir -p "$configs_dir/plex"
+mkdir -p "$configs_dir/sabnzbd"
+mkdir -p "$configs_dir/sonarr"
+mkdir -p "$configs_dir/radarr"
+
+mkdir -p "$plex_dir"
+mkdir -p "$plex_dir/movies"
+mkdir -p "$plex_dir/tv"
+mkdir -p "$plex_dir/photos"
+
+mkdir -p "$sabnzbd_dir"
+mkdir -p "$sabnzbd_dir/movies"
+mkdir -p "$sabnzbd_dir/tv"
+
+mkdir -p "$sonarr_dir"
+mkdir -p "$sonarr_dir/complete"
+mkdir -p "$sonarr_dir/incomplete"
+
+mkdir -p "$radarr_dir"
+mkdir -p "$radarr_dir/complete"
+mkdir -p "$radarr_dir/incomplate"
