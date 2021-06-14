bcc44a3595a5382dbdb89735b2cb7098d5743a4b
Dennis Stine <dennis.stine@gmail.com>
2021-06-14 00:27:20 -0500

    Create open_firewall.sh

---
+++ open_firewall.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sudo firewall-cmd --list-all
+
+sudo firewall-cmd --zone=public --permanent --add-service=http
+sudo firewall-cmd --zone=public --permanent --add-service=https
+
+# plex ui port - others can be opened as well
+sudo firewall-cmd --zone=public --permanent --add-port 32400/tcp
+
+# sabnzbd
+sudo firewall-cmd --zone=public --permanent --add-port 8080/tcp
+sudo firewall-cmd --zone=public --permanent --add-port 9090/tcp
+
+# sonarr
+sudo firewall-cmd --zone=public --permanent --add-port 8989/tcp
+
+# radarr
+sudo firewall-cmd --zone=public --permanent --add-port 7878/tcp
+
+sudo firewall-cmd --reload
+
+sudo firewall-cmd --list-all
